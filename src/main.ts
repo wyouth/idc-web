a6250b322a775714999fe3279fbe1876eddfb72b
wang <wang@wangdeMac-Pro.bbrouter>
2023-04-18 22:53:04 +0800

    Add 初始化项目

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+// import {HttpExceptionFilter} from "./core/filter/http-exception/http-exception.filter";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  // app.useGlobalInterceptors(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
